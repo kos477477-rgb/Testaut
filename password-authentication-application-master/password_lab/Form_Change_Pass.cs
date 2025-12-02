733d454cfecb17cf55d4163055afef7632f98d98
kos477477-rgb <kos477477@gmail.com>
2025-12-02 13:06:09 +0500

    Первая версия

---
+++ password-authentication-application-master/password_lab/Form_Change_Pass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace password_lab
+{
+    public partial class Form_Change_Pass : Form
+    {
+        public Form_Change_Pass()
+        {
+            InitializeComponent();
+
+        }
+
+    }
+}
---
+++ password-authentication-application-master/password_lab/Form_Pass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace password_lab
+{
+    public partial class Form_Pass : Form
+    {
+
+        public Form_Pass()
+        {
+            InitializeComponent();
+
+        }
+
+    }
+}
